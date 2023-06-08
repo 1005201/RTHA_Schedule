423379c2dc25fb8f87caa58fb9af7816dbe9bb81
Diana <1005201@mborijnland.nl>
2023-06-08 10:17:41 +0200

    update admin

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +18,11 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class)
+
+
+
+            //agree terms
             /*->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -43,7 +48,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('repeatPassword', PasswordType::class, [
+            /*->add('repeatPassword', PasswordType::class, [
 // instead of being set onto the object directly,
 // this is read and encoded in the controller
                 'mapped' => false,
@@ -59,7 +64,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
+            ])*/
         ;
     }
 
