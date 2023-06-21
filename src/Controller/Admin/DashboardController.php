0588f7ecb3e5885ceb9e265025afba5a1d32c330
Diana <1005201@mborijnland.nl>
2023-06-21 13:10:08 +0200

    update DashboardController.php: via admin-page naar website-page

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Available;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -42,6 +43,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linktoRoute('Website','fas fa-backward','app_default');
         yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+
     }
 }
